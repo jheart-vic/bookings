7d837ab8e6ea74a3929489638f6e2ea096465281
jheart-vic <adebowalevictorjheart@gmail.com>
2023-10-07 22:50:58 +0100

    modify app

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: "https://bookings-575t2vbnn-victor-chiemerie-akabuezes-projects.vercel.app",
+    origin: "*",
   })
 );
 
