aea31ad197cf4611077e8d730c6eeb0e418d0c45
jheart-vic <adebowalevictorjheart@gmail.com>
2023-10-18 16:02:30 +0100

    modify .env

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,15 +1,24 @@
 /* eslint-disable react/prop-types */
-export default function Image({ src, ...rest }) {
-  const baseUrl = import.meta.env.VITE_BASE_URL || 'http://localhost:4000';
+// export default function Image({ src, ...rest }) {
+//   const baseUrl = import.meta.env.VITE_BASE_URL || 'http://localhost:4000';
 
-  if (src && src.startsWith('https://')) {
-    return (
-      <img {...rest} src={src} alt={''} />
-    );
-  } else {
-    const localSrc = `${baseUrl}/uploads/${src}`;
-    return (
-      <img {...rest} src={localSrc} alt={''} />
-    );
-  }
+//   if (src && src.startsWith('https://')) {
+//     return (
+//       <img {...rest} src={src} alt={''} />
+//     );
+//   } else {
+//     const localSrc = `${baseUrl}/uploads/${src}`;
+//     return (
+//       <img {...rest} src={localSrc} alt={''} />
+//     );
+//   }
+// }
+
+export default function Image({ src, ...rest }) {
+  const isExternalUrl = src.startsWith('http://') || src.startsWith('https://');
+
+  // Construct the image URL based on whether it's external or local
+  const imageUrl = isExternalUrl ? src : `${import.meta.env.VITE_BASE_URL}/uploads/${src}`;
+
+  return <img {...rest} src={imageUrl} alt={''} />;
 }
